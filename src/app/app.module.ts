db779f1e83ced15abb6802e3182e14e5ca7a0b4a
Jed Patrick Datu <jedpatrickdatu@gmail.com>
2019-05-21 13:59:56 +0800

    Positions the homepage's main background image and header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DonorsComponent } from './donors/donors.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import {MatButtonModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatButtonToggleModule, MatBadgeModule, MatProgressSpinnerModule, MatListModule, MatGridList, MatGridListModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
+const material = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DonorsComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
+    material,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
