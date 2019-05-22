5182cd394e1db5ab2e9c9894e05cbe45998b6629
Jed Patrick Datu <jedpatrickdatu@gmail.com>
2019-05-22 09:55:23 +0800

    Removes space between navbar and the main content of the homepage.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DonorsComponent } from './donors/donors.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import {MatButtonModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatButtonToggleModule, MatBadgeModule, MatProgressSpinnerModule, MatListModule, MatGridList, MatGridListModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -23,7 +22,6 @@ const material = [
   declarations: [
     AppComponent,
     DonorsComponent,
-    NavigationBarComponent,
     MainNavComponent
   ],
   imports: [
